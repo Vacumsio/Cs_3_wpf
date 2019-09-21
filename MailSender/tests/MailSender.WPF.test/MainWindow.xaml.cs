89886d91a6b8c340f66b284e0be9edd2dbef7469
alaska <vacumsio@gmail.com>
2019-09-21 12:46:29 +0300

    keep learning

--- MailSender/tests/MailSender.WPF.test/MainWindow.xaml.cs
+++ MailSender/tests/MailSender.WPF.test/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Net.Mail;
+using System.Security;
+using System.Net;
+using System;
 
 namespace MailSender.WPF.test
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void SendButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var user_name = "";
+            SecureString password = null;
+            var host = "smtp.yandex.ru";
+            var port = 25;
+
+            var msg = "Hello world"+ DateTime.Now;
+
+            using (var client = new SmtpClient(host, port))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(user_name, password);
+
+                using(var message = new MailMessage())
+                {
+                    message.From = new MailAddress("peter.gagarsky@yandex.ru","Пётр");
+                }
+            }
+        }
     }
-}
+} 

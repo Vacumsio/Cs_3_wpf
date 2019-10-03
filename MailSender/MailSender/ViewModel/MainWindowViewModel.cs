9cd5c95125dc9dde22fbfb4613258761b0cac645
alaska <vacumsio@gmail.com>
2019-10-03 14:56:40 +0300

    проблемы с реализацией mvvmlight на версии 4.7.2 перевел проект на версию 4.6.2

---
+++ MailSender/MailSender/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace MailSender.ViewModel
+{
+    public class MainWindowViewModel :ViewModelBase
+    {
+        private string _WindowTitle = "Рассыльщик почты версии 0.001";
+
+        public string WindowTitle
+        {
+            get => _WindowTitle;
+            set => Set(ref _WindowTitle, value);
+        }
+    }
+}
